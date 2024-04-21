2bbff8c367af5f3eb25743c46cc94fd8fa44d288
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-04-21 07:30:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Get the current URL
+var currentURL = window.location.href;
+
+// Get the length of the URL
+var urlLength = currentURL.length;
+
+// Print the length of the URL using alert
+alert("The length of the URL is: " + urlLength);
